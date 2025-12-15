bf239a23160da863e52116128e3fe62542588fb9
gmldnjs9916-creator <gmldnjs9916@gmail.com>
2025-12-15 16:23:20 +0900

    Update telegram_ctifeeds.py

--- app/telegram_ctifeeds.py
+++ app/telegram_ctifeeds.py
@@ -8,7 +8,6 @@ import re
 from .models import IntermediateEvent, LeakRecord
 from .enrich_with_osint import enrich_leakrecord_with_osint
 
-
 # --------------------------------------------------------
 # URL → 도메인 추출 유틸
 # --------------------------------------------------------
@@ -89,3 +88,4 @@ def intermediate_to_leakrecord(event: IntermediateEvent) -> LeakRecord:
 
     # OSINT 보강
     return enrich_leakrecord_with_osint(record)
+
